cbbad88416829ebc41eebfcc91109b920fd4f43d
Davi Arruda Navarro Albuquerque <89603106+davinavarro998@users.noreply.github.com>
2023-04-26 16:29:20 -0300

    update script
    update with cat fetch data

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const targetFact = document.getElementById("target-fact");
+const catButton = document.getElementById("cat-button");
+
 document.addEventListener("DOMContentLoaded", function () {
-    const contactLink = document.getElementById('contact-link');
+    const contactLink = document.getElementById("contact-link");
 
     contactLink.addEventListener("click", function (event) {
         event.preventDefault();
@@ -8,7 +11,28 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function toggleMenu() {
-  const navLinks = document.querySelector(".nav-links");
-  navLinks.classList.toggle("show");
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("show");
 }
+async function fetchData(){
+    try {
+        const response = await fetch("https://catfact.ninja/fact");
+        if (response.ok) {
+            const data = await response.json();
+            return data.fact;
+        } else {
+            console.error("Error fetching cat fact:", response.statusText);
+            return "Failed to fetch cat fact";
+        }
+    } catch (error) {
+        console.error("Error fetching cat fact:", error);
+        return "Failed to fetch cat fact";
+    }
+}
+
+
+catButton.addEventListener("click", async () => {
+    const result = await fetchData();
+    targetFact.textContent = result;
+});
 
